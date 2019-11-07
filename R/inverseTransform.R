597511c589d740b7ac247a7b6967c0e51f30da82
Weiming <weiming@psu.edu>
2019-11-07 18:37:26 -0500

    Update inverseTransform.R

--- R/inverseTransform.R
+++ R/inverseTransform.R
@@ -44,7 +44,8 @@ inverseTransform <- function(
   # Sanity checks
   stopifnot(length(dim(analogs)) == 4)
   if (any(heuristic.rank == 0)) {
-    stop('Some bins end up with 0 counts. This can be caused by too large ensemble.')
+    warning('Some bins end up with 0 counts. This can be caused by too large ensemble. The process is terminated.')
+    return(NULL)
   }
   
   if (length(heuristic.rank) != dim(analogs)[4] + 1) {
